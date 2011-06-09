87e344b1cbbd76eca7736df1a894a91d91cee87a
Samuel Toriel <samueltoriel@gmail.com>
2011-06-09 18:01:27 -0400

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-customreport',
+    version="0.0",
+    author='Samuel Toriel',
+    author_email='samueltoriel@gmail.com',
+    description='Select fields / relations given an app label and model label',
+    url='http://github.com/riltsken/django-relation-selector',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)
