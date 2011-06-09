b338e65293699a3f6d330a7dfd00ff41e8249fe0
Samuel Toriel <samueltoriel@gmail.com>
2011-06-09 18:07:00 -0400

    rename project in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-customreport',
+    name='django-relation-selector',
     version="0.0",
     author='Samuel Toriel',
     author_email='samueltoriel@gmail.com',
